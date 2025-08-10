17d1f03cacacd35cc813a34bb3adef7fc93b1905
Vishnu4538 <edlavishnu@aol.com>
2025-08-10 18:58:16 +0530

    Update app.py
    ok

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def get_products():
     return jsonify([
         {"id": 101, "name": "Laptop"},
-        {"id": 102, "name": "vishnu"}
+        {"id": 102, "name": "kalyan"}
     ])
 
 if __name__ == '__main__':
