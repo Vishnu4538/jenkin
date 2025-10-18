0d4dab1d2ba4bd190ad50e70dd067114952dceee
Vishnu4538 <edlavishnu@aol.com>
2025-10-18 12:18:49 +0530

    Fix typo in commit message and echo statement
    Edited commit line in script

--- image_update.sh
+++ image_update.sh
@@ -15,9 +15,9 @@ sed -i "19i\        image: docker.io/$1/nginxapp:$2" vishnu.yml
 
 git add .
 
-git commit -m "Imaged is update to $2"
+git commit -m "Image is version updated to $2"
 
-echo "We have commit tag change to $2"
+echo "We have committed tag change to $2"
 
 git push origin main 
 
