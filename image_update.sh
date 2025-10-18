245b3206a07a662084559700df797e1fc5d01dc0
vishnu <edlavishnu@aol.com>
2025-10-18 06:19:55 +0000

    modified image script

--- image_update.sh
+++ image_update.sh
@@ -9,15 +9,15 @@ sed -i {"/image:/d"} vishnu.yml
 
 sleep 9s
 
+Adding image line to file $1 is username of docker and $2 is for tag
 
-adding image line to file $1 is username of docker and $2 is for tag
 sed -i "19i\        image: docker.io/$1/front:$2" vishnu.yml
 
-git add vishnu.yml
+git add .
 
-git commit -m "Imaged is update to $1"
+git commit -m "Imaged is update to $2"
 
-echo "We have commit tag change to $1"
+echo "We have commit tag change to $2"
 
 git push origin main 
 
